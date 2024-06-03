2237a5083bd0399b0f176e9c2cc81d7bc9266822
Stephen <stephen.j.herzog@gmail.com>
2024-06-03 15:00:06 -0400

    DeleteComment updated

---
+++ vue/src/services/PostService.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+const http = axios.create ({
+    baseURL: "http://localhost:9000"
+});
+
+export default {
+
+  getAllPosts() {
+    return axios.get('/posts')
+  },
+
+  getPost(postId) {
+    return axios.get(`/post/${postId}`)
+  },
+
+  updatePost(postID) {
+    return axios.put(`/update-post`, postID);
+  },
+
+  addPost(post) {
+    return http.post('add-post', post)
+  }
+
+}
