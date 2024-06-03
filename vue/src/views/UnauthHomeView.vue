12651cf739d8ea40c19d6e33f9cdd5e13e1c4d9f
Cody Osei <cody.osei@gmail.com>
2024-06-03 12:12:46 -0700

    Created Unauthorized HomeView and ForumsCard

---
+++ vue/src/views/UnauthHomeView.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="home">
+      <h1>Home</h1>
+      <p></p>
+    </div>
+  </template>
+  
+  <script>
+
+import ForumService from '../services/ForumService'
+
+  export default {
+    components: {
+
+    },
+    data(){
+        return {
+             forum: []
+        };
+    },
+    methods: {
+        getForums(){
+            ForumService.getAllForums().then((response) =>{
+                this.forum = response.data;
+            })
+            .catch(error =>{
+                
+            })
+        }
+    },
+    created(){
+        this.getForums();
+    }
+  };
+  </script>
+  
