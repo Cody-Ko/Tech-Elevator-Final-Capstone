4fc5884380acf25d52ada81e723079e289658be8
Matthew Cable <matthewcable91@gmail.com>
2024-06-03 15:31:16 -0400

    created data model classes, they are still mostly empty

---
+++ java/src/main/java/com/techelevator/model/Comment.java
@@ -0,0 +1,14 @@
+package com.techelevator.model;
+
+import java.util.Date;
+
+public class Comment {
+
+    private int userID;
+    private int replyToID;
+    private int postID;
+    private String messageDetails;
+    private Date timeStamp;
+
+
+}
