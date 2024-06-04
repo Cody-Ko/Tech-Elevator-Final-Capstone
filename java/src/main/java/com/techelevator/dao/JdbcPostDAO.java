ace192da3ffe0d37c1ed12c4125e6e69c62f5905
Matthew Cable <matthewcable91@gmail.com>
2024-06-04 11:18:33 -0400

    Framing out DAO methods, still need to fill in method bodies

--- java/src/main/java/com/techelevator/dao/JdbcPostDAO.java
+++ java/src/main/java/com/techelevator/dao/JdbcPostDAO.java
@@ -1,6 +1,70 @@
 package com.techelevator.dao;
 
+import com.techelevator.model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JdbcPostDAO implements PostDAO {
 
+    /*** SINGLE POST GETTERS ***/
+
+    public Post getPostByID(int postID){
+        return new Post();
+    }
+    public Post getPostByTitle(String postTitle){
+        return new Post();
+    }
+    public int getUserID(int postID){
+
+        return 999;
+    }
+    public String getUserName(int postID){
+        return new String();
+    }
+
+    /*** LIST POST GETTERS ***/
+    public List<Post> getAllPosts(){
+        List<Post> rtnList = new ArrayList<Post>();
+        return rtnList;
+    }
+    public List<Post> getPostsByUsername(String Username){
+        List<Post> rtnList = new ArrayList<Post>();
+        return rtnList;
+    }
+    public List<Post> getPostsByUserID(int userID){
+        List<Post> rtnList = new ArrayList<Post>();
+        return rtnList;
+    }
+    public List<Post> getPostsByForumName(String forumName){
+        List<Post> rtnList = new ArrayList<Post>();
+        return rtnList;
+    }
+    public List<Post> getPostsByForumID(int forumID){
+        List<Post> rtnList = new ArrayList<Post>();
+        return rtnList;
+    }
+
+    /*** CREATE AND DELETE POSTS ***/
+    public void createPost(Post toPost){
+
+    }
+    public void deletePost(int postID){
+
+    }
+    public void deletePostsByUserID(int userID){
+
+    }
+
+    /*** METHODS FOR VOTING ON POSTS ***/
+    public void upvotePost(int postID){
+
+    }
+    public void downvotePost(int postID){
+
+    }
+    public int getPostScore(int postID){
+        return 999;
+    }
 
 }
--- java/src/main/java/com/techelevator/model/Post.java
+++ java/src/main/java/com/techelevator/model/Post.java
@@ -3,12 +3,15 @@ import java.util.Date;
 
 public class Post {
 
+    private int postID;
     private int userID;
+    private int forumID;
+    private String title;
     private String messageDetails;
     private int upVotes;
     private int downVotes;
     private Date timeStamp;
-    private String title;
+
 
 
 
