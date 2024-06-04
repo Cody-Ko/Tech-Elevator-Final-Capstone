2caacfd48313f490f9a1c1b91ec549e9150f375e
Kyndall Kilgore <kyndallf25@gmail.com>
2024-06-04 08:25:21 -0500

    database updated: favorite forums added + forum id to posts + spelling error

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -33,19 +33,29 @@ CREATE TABLE forum (
 CREATE TABLE posts (
 	post_id SERIAL PRIMARY KEY,
     	user_id INT NOT NULL,
+    	forum_id INT NOT NULL,
     	title VARCHAR(50) NOT NULL,
     	message VARCHAR(1000) NOT NULL,
     	up_votes INT NOT NULL,
     	down_votes INT NOT NULL,
     	time_stamp TIMESTAMP NOT NULL,
+    	FOREIGN KEY (forum_id) REFERENCES forum(forum_id),
     	FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
-CREATE TABLE user_form (
+CREATE TABLE user_forum (
 	user_id INT NOT NULL,
     	forum_id INT NOT NULL,
     	PRIMARY KEY (user_id, forum_id),
     	FOREIGN KEY (user_id) REFERENCES users(user_id),
     	FOREIGN KEY (forum_id) REFERENCES forum(forum_id)
 );
+
+CREATE TABLE user_favorite_forum (
+    forum_id INT NOT NULL,
+    forum_user_id INT NOT NULL
+    PRIMARY KEY (forum_id, forum_user_id),
+    FOREIGN KEY (forum_user_id) REFERENCES users(user_id),
+    FOREIGN KEY (forum_id) REFERENCES forum(forum_id),
+)
 
 COMMIT TRANSACTION;
