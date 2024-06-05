be22806600275823cc9d6bba47e3e16a0babed17
Stephen <stephen.j.herzog@gmail.com>
2024-06-05 11:39:09 -0400

    Post card updated, router updated

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,8 +6,9 @@ import HomeView from '../views/HomeView.vue';
 import LoginView from '../views/LoginView.vue';
 import LogoutView from '../views/LogoutView.vue';
 import RegisterView from '../views/RegisterView.vue';
-import UnauthHomeView from '../views/UnauthHomeView.vue'
-import ForumView from '../views/ForumView.vue'
+import UnauthHomeView from '../views/UnauthHomeView.vue';
+import ForumView from '../views/ForumView.vue';
+import PostView from '../views/PostView.vue'
 
 /**
  * The Vue Router is used to "direct" the browser to render a specific view component
@@ -66,6 +67,14 @@ const routes = [
       requiresAuth: false
     }
   },
+  {
+    path: "/posts",
+    name: "postview",
+    component: PostView,
+    meta: {
+      requiresAuth: false
+    }
+  }
 ];
 
 // Create the router
---
+++ vue/src/views/PostView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="post">
+    Welcome to Posts
+  </div>
+</template>
+
+<script>
+import PostService from '../services/PostService';
+
+export default {
+    data() {
+        return {
+            post: {},
+            postId: this.$route.params.postId,
+        };
+    },
+    methods: {
+        getPosts() {
+            PostService.getAllPosts().then(
+                (response) => {
+                    this.post = response.data;
+                }
+            )
+        },
+        getPost() {
+            PostService.getPost().then(
+                (response) => {
+                    this.post.response.data;
+                }
+            )
+        },
+    }
+
+}
+</script>
+
+<style>
+
+</style>
