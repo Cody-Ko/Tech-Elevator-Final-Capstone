fe3d4410dc037ef1a7ba5486635c467d1fdf1cad
Stephen <stephen.j.herzog@gmail.com>
2024-06-10 21:52:36 -0400

    Create post card, searches fixed

--- vue/src/components/CreatePostCard.vue
+++ vue/src/components/CreatePostCard.vue
@@ -1,13 +1,16 @@
 <template>
 
 <div class="create-post-home"> Create A Post</div>
-    <form v-on:submit.prevent="addPost">
+    <form class ="build-a-post" v-on:submit.prevent="addPost">
       
       <label for="post_name">Name of Post</label>
       <input type="text" id="post_name" name="post_name" v-model="changePost.title"/>
 
       <label for="post_message_details">Message of Post</label>
       <input type="text" id="post_message_details" name="post_message_details" v-model="changePost.message"/>
+
+      <label for="post_location">Location of Post</label>
+      <input type="text" id="post_location" name="post_location" v-model="changePost.location"/>
       
       <div class="submissions">
         <button class="submitBtn" type="submit">Submit</button>
@@ -39,6 +42,7 @@ export default {
         //   forum_user_id: 0,
           title: '',
           message: '',
+          location: '',
         //   time_stamp: Date,
           
         },
@@ -66,28 +70,42 @@ export default {
   </script>
   
   <style scoped>
-  .PostCard {
+  .create-post-home {
       display: grid;
-      grid-template-columns: auto 1fr;
-      grid-template-areas: 
-      "vote-icons post-name"
-      "message message"
-      "time time";
+      align-content: center;
       /* flex-direction: column;
       justify-content: space-between; */
       border-radius: 15px;
-      width: 50%;
       /* max-width: 750px; */
-      min-width: 600px;
-      /* height: 250px; */
-      min-height: 500px;  
+      /* height: 250px; */ 
       /* margin: 20px; */
       margin: 0 auto;   /* this keeps the card in the middle of the page */
       margin-bottom: 1%;
-      border: 8px solid rgb(250, 129, 240);
+      font-size: 2.5rem;
+      font-weight: bold;
+      color: white;
+      margin-top: .25%;
+      text-align: center;
+  }
+
+  .build-a-post {
+      display: grid;
+      align-content: center;
+      /* flex-direction: column;
+      justify-content: space-between; */
+      border-radius: 15px;
+      justify-content: space-around;
+      /* width: 50%; */
+      /* max-width: 750px; */
+      /* min-width: 600px; */
+      /* height: 250px; */
+      /* min-height: 500px;   */
+      /* margin: 20px; */
+      margin: 0 auto;   /* this keeps the card in the middle of the page */
+      margin-bottom: 1%;
       /* font-size: 2.5rem; */
       color: white;
-      margin-top: .5%;
+      margin-top: .25%;
       text-align: center;
       padding: 10px;
       /* background-image: ; */
