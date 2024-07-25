beffffbbb4f21e201065867709b8185f37bb0f64
Cody-Ko <cody.osei@gmail.com>
2024-07-24 22:55:30 -0700

    Updates to application.properties

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -3,11 +3,23 @@ package com.techelevator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
+public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(Application.class);
+    app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
+    app.run(args);
+}
+
+    private static String getPort() {
+        String port = System.getenv("PORT");
+        return port != null ? port : "8080";
     }
 
 }
