6e3097827cde68d368d3638fead0431c3a092550
Cody-Ko <cody.osei@gmail.com>
2024-07-25 15:46:37 -0700

    Updates to merge github frontend and heroku backend

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -3,7 +3,7 @@ package com.techelevator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
+//import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
@@ -13,13 +13,6 @@ public class Application {
 //    }
 public static void main(String[] args) {
     SpringApplication app = new SpringApplication(Application.class);
-    app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
     app.run(args);
 }
-
-    private static String getPort() {
-        String port = System.getenv("PORT");
-        return port != null ? port : "https://stellar-discussions-142a61c436e5.herokuapp.com/";
-    }
-
 }
