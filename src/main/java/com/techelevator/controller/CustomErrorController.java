68a7b59a3110e67e1cef4b3f5386a73bdc52ee33
Cody-Ko <cody.osei@gmail.com>
2024-07-24 23:13:17 -0700

    Updates to Controllers

---
+++ src/main/java/com/techelevator/controller/CustomErrorController.java
@@ -0,0 +1,25 @@
+package com.techelevator.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleError() {
+        // Return a custom 404 error page
+        return "The page you are looking for does not exist.";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
