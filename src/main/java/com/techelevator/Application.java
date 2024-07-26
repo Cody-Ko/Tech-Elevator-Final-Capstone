e3f08eccbfe765a1bc2a653b5227136c9b969ca1
Cody-Ko <cody.osei@gmail.com>
2024-07-25 17:34:56 -0700

    Updates to merge github frontend and heroku backend

--- src/main/java/com/techelevator/Application.java
+++ src/main/java/com/techelevator/Application.java
@@ -3,7 +3,7 @@ package com.techelevator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import java.util.Collections;
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
@@ -11,8 +11,21 @@ public class Application {
 //    public static void main(String[] args) {
 //        SpringApplication.run(Application.class, args);
 //    }
+
+//    public static void main(String[] args) {
+//        SpringApplication app = new SpringApplication(Application.class);
+//        app.run(args);
+//    }
+//}
 public static void main(String[] args) {
     SpringApplication app = new SpringApplication(Application.class);
+    app.setDefaultProperties(Collections.singletonMap("server.port", getPort()));
     app.run(args);
 }
+
+    private static String getPort() {
+        String port = System.getenv("PORT");
+        return port != null ? port : "${PORT:8080}";
+    }
+
 }
